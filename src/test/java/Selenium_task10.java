92fbd632b5c69a37afe9f5c1229949e7221a7e6e
perceva <ekaterina_perets@mail.ru>
2018-03-31 17:13:34 +0300

    10 задание 3 браузера.

--- src/test/java/Selenium_task10.java
+++ src/test/java/Selenium_task10.java
@@ -1,35 +1,257 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfAllElementsLocatedBy;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class Selenium_task10 extends baseTest {
 
-    public static final String URL = "http://localhost/litecart/";
+    public static final String URL = "http://localhost/litecart/en/";
+    WebDriver driver;
+    WebDriverWait wait;
 
     @Test
-    public void rightPage(){
-      driver.get(URL);
-        String prMain = driver.findElement(By.xpath("//s[@class='regular-price']")).getText();
+    public void ChromerightPage() {
+
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, 15);
+            driver.get(URL);
+
+            List<WebElement> rows = wait.until(visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='box-campaigns']//li")));
+
+            for (int i = 0; i < rows.size(); i++) {
+
+
+                rows = wait.until(visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='box-campaigns']//li")));
+
+                WebElement priceRegularMainW = rows.get(i).findElement(By.xpath("//s[@class='regular-price']"));
+                WebElement newCampaignMainW = rows.get(i).findElement(By.xpath("//strong[@class='campaign-price']"));
+                WebElement nameW = rows.get(i).findElement(By.xpath("//div[@id='box-campaigns']//li//div[@class='name']"));
+
+                String priceRegularMain = priceRegularMainW.getText();
+                String newCampaignMain = newCampaignMainW.getText();
+                String name = nameW.getAttribute("textContent");
+
+                System.out.println(name + " " + newCampaignMain + " " + priceRegularMain);
+
+                Dimension prRegM = priceRegularMainW.getSize();
+                Dimension newCamM = newCampaignMainW.getSize();
+
+                Assert.assertTrue((newCamM.height > prRegM.height & newCamM.width > prRegM.width), "Wrong size");
+                Assert.assertTrue(checkGrayColor(priceRegularMainW), "Not gray");
+                Assert.assertTrue(checkRedColor(newCampaignMainW), "Not red");
+
+                System.out.println(newCampaignMainW.getCssValue("font-weight"));
+                System.out.println(priceRegularMainW.getCssValue("text-decoration-line"));
+
+                Assert.assertTrue((priceRegularMainW.getCssValue("text-decoration-line")
+                        .equals("line-through")), "Wrong line stile");
+                Assert.assertTrue((newCampaignMainW.getCssValue("font-weight")
+                        .equals("700")), "Wrong font weight");
+
+                rows.get(i).click();
+
+                String duckPage = wait.until(visibilityOfElementLocated(By.cssSelector("h1.title")))
+                        .getAttribute("textContent");
+                System.out.println(duckPage);
+
+                WebElement regularPrice = wait.until(visibilityOfElementLocated(By.cssSelector(".regular-price")));
+                WebElement campaignPrice = wait.until(visibilityOfElementLocated(By.cssSelector(".campaign-price")));
+
+                String regularPriceS = regularPrice.getAttribute("textContent");
+                String campaignPriceS = campaignPrice.getAttribute("textContent");
+                Dimension regPr = regularPrice.getSize();
+                Dimension cmpPr = campaignPrice.getSize();
+
+                Assert.assertTrue(name.equals(duckPage), "Invalid header");
+                Assert.assertTrue(priceRegularMain.equals(regularPriceS), "Invalid regular price");
+                Assert.assertTrue(newCampaignMain.equals(campaignPriceS), "Invalid new price");
+                Assert.assertTrue(checkGrayColor(regularPrice), "Not gray");
+                Assert.assertTrue(checkRedColor(campaignPrice), "Not red");
+                Assert.assertTrue((regularPrice.getCssValue("text-decoration-line")
+                        .equals("line-through")), "Wrong line stile");
+                Assert.assertTrue((campaignPrice.getCssValue("font-weight")
+                        .equals("700")), "Wrong font weight");
+                Assert.assertTrue((cmpPr.height > regPr.height & cmpPr.width > regPr.width), "Wrong size");
+
+
+                wait.until(visibilityOfElementLocated(By.cssSelector(".general-0"))).click();
+                System.out.println("everything is ok");
+            }
+
+
+
+
+    }
+
+    @Test
+    public void FirefoxrightPage() {
+
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 15);
+        driver.get(URL);
 
         List<WebElement> rows = wait.until(visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='box-campaigns']//li")));
 
-        for(int i=0; i<rows.size();i++){
+        for (int i = 0; i < rows.size(); i++) {
+
+
             rows = wait.until(visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='box-campaigns']//li")));
-            String priceMain = rows.get(i).findElement(By.xpath("//s[@class='regular-price']")).getText();
-            String newPriceMain = rows.get(i).findElement(By.xpath("//strong[@class='campaign-price']")).getText();
-            String name = rows.get(i).findElement(By.xpath("//div[@class='name']")).getText();
-            System.out.println(priceMain);
+
+            WebElement priceRegularMainW = rows.get(i).findElement(By.xpath("//s[@class='regular-price']"));
+            WebElement newCampaignMainW = rows.get(i).findElement(By.xpath("//strong[@class='campaign-price']"));
+            WebElement nameW = rows.get(i).findElement(By.xpath("//div[@id='box-campaigns']//li//div[@class='name']"));
+
+            String priceRegularMain = priceRegularMainW.getText();
+            String newCampaignMain = newCampaignMainW.getText();
+            String name = nameW.getAttribute("textContent");
+
+            System.out.println(name + " " + newCampaignMain + " " + priceRegularMain);
+
+            Dimension prRegM = priceRegularMainW.getSize();
+            Dimension newCamM = newCampaignMainW.getSize();
+
+            Assert.assertTrue((newCamM.height > prRegM.height & newCamM.width > prRegM.width), "Wrong size");
+            Assert.assertTrue(checkGrayColor(priceRegularMainW), "Not gray");
+            Assert.assertTrue(checkRedColor(newCampaignMainW), "Not red");
+
+            System.out.println(newCampaignMainW.getCssValue("font-weight"));
+            System.out.println(priceRegularMainW.getCssValue("text-decoration-line"));
+
+            Assert.assertTrue((priceRegularMainW.getCssValue("text-decoration-line")
+                    .equals("line-through")), "Wrong line stile");
+            Assert.assertTrue((newCampaignMainW.getCssValue("font-weight")
+                    .equals("900")), "Wrong font weight");
+
+            rows.get(i).click();
+
+            String duckPage = wait.until(visibilityOfElementLocated(By.cssSelector("h1.title")))
+                    .getAttribute("textContent");
+            System.out.println(duckPage);
+
+
+            WebElement regularPrice = wait.until(visibilityOfElementLocated(By.cssSelector(".regular-price")));
+            WebElement campaignPrice = wait.until(visibilityOfElementLocated(By.cssSelector(".campaign-price")));
+            System.out.println(campaignPrice.getCssValue("font-weight"));
+
+            String regularPriceS = regularPrice.getAttribute("textContent");
+            String campaignPriceS = campaignPrice.getAttribute("textContent");
+            Dimension regPr = regularPrice.getSize();
+            Dimension cmpPr = campaignPrice.getSize();
+
+            Assert.assertTrue(name.equals(duckPage), "Invalid header");
+            Assert.assertTrue(priceRegularMain.equals(regularPriceS), "Invalid regular price");
+            Assert.assertTrue(newCampaignMain.equals(campaignPriceS), "Invalid new price");
+            Assert.assertTrue(checkGrayColor(regularPrice), "Not gray");
+            Assert.assertTrue(checkRedColor(campaignPrice), "Not red");
+            Assert.assertTrue((regularPrice.getCssValue("text-decoration-line")
+                    .equals("line-through")), "Wrong line stile");
+            Assert.assertTrue((campaignPrice.getCssValue("font-weight")
+                    .equals("700")), "Wrong font weight");
+            Assert.assertTrue((cmpPr.height > regPr.height & cmpPr.width > regPr.width), "Wrong size");
+
+
+            wait.until(visibilityOfElementLocated(By.cssSelector(".general-0"))).click();
+            System.out.println("everything is ok");
         }
 
     }
 
+    @Test
+    public void edgePage() {
 
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        wait = new WebDriverWait(driver, 15);
+        driver.get(URL);
+
+        List<WebElement> rows = wait.until(visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='box-campaigns']//li")));
+
+        for (int i = 0; i < rows.size(); i++) {
+
+
+            rows = wait.until(visibilityOfAllElementsLocatedBy(By.xpath("//div[@id='box-campaigns']//li")));
+
+            WebElement priceRegularMainW = rows.get(i).findElement(By.xpath("//s[@class='regular-price']"));
+            WebElement newCampaignMainW = rows.get(i).findElement(By.xpath("//strong[@class='campaign-price']"));
+            WebElement nameW = rows.get(i).findElement(By.xpath("//div[@id='box-campaigns']//li//div[@class='name']"));
+
+            String priceRegularMain = priceRegularMainW.getText();
+            String newCampaignMain = newCampaignMainW.getText();
+            String name = nameW.getAttribute("textContent");
+
+            System.out.println(name + " " + newCampaignMain + " " + priceRegularMain);
+
+            Dimension prRegM = priceRegularMainW.getSize();
+            Dimension newCamM = newCampaignMainW.getSize();
+
+            Assert.assertTrue((newCamM.height > prRegM.height & newCamM.width > prRegM.width), "Wrong size");
+            Assert.assertTrue(checkGrayColor(priceRegularMainW), "Not gray");
+            Assert.assertTrue(checkRedColor(newCampaignMainW), "Not red");
+
+            System.out.println(newCampaignMainW.getCssValue("font-weight"));
+            System.out.println(priceRegularMainW.getCssValue("text-decoration"));
+
+            Assert.assertTrue((priceRegularMainW.getCssValue("text-decoration")
+                    .equals("line-through")), "Wrong line stile");
+            Assert.assertTrue((newCampaignMainW.getCssValue("font-weight")
+                    .equals("700")), "Wrong font weight");
+
+            rows.get(i).click();
+
+            String duckPage = wait.until(visibilityOfElementLocated(By.cssSelector("h1.title")))
+                    .getAttribute("textContent");
+            System.out.println(duckPage);
+
+
+            WebElement regularPrice = wait.until(visibilityOfElementLocated(By.cssSelector(".regular-price")));
+            WebElement campaignPrice = wait.until(visibilityOfElementLocated(By.cssSelector(".campaign-price")));
+            System.out.println(campaignPrice.getCssValue("font-weight"));
+
+            String regularPriceS = regularPrice.getAttribute("textContent");
+            String campaignPriceS = campaignPrice.getAttribute("textContent");
+            Dimension regPr = regularPrice.getSize();
+            Dimension cmpPr = campaignPrice.getSize();
+
+            Assert.assertTrue(name.equals(duckPage), "Invalid header");
+            Assert.assertTrue(priceRegularMain.equals(regularPriceS), "Invalid regular price");
+            Assert.assertTrue(newCampaignMain.equals(campaignPriceS), "Invalid new price");
+            Assert.assertTrue(checkGrayColor(regularPrice), "Not gray");
+            Assert.assertTrue(checkRedColor(campaignPrice), "Not red");
+            Assert.assertTrue((regularPrice.getCssValue("text-decoration")
+                    .equals("line-through")), "Wrong line stile");
+            Assert.assertTrue((campaignPrice.getCssValue("font-weight")
+                    .equals("700")), "Wrong font weight");
+            Assert.assertTrue((cmpPr.height > regPr.height & cmpPr.width > regPr.width), "Wrong size");
+
+
+            wait.until(visibilityOfElementLocated(By.cssSelector(".general-0"))).click();
+            System.out.println("everything is ok");
+        }
+
+    }
 }
+
+
+
+
+
+
 
 
--- src/test/java/baseTest.java
+++ src/test/java/baseTest.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class baseTest {
@@ -47,6 +50,36 @@ public WebDriverWait wait;
     public WebElement visibilityOfElement(By locator){
         WebElement element =  this.wait.until(ExpectedConditions.elementToBeClickable(locator));
         return element;
+    }
+
+    public Boolean checkGrayColor(WebElement w){
+        String colorOld = w.getCssValue("color");
+        String c = colorOld.substring(colorOld.indexOf("("), colorOld.indexOf(")")).replaceAll(",", "");
+        String formattedOne = c.split(" ")[0].replace("(", "");
+        String formattedTwo =c.split(" ")[1];
+
+        String formatted3 =c.split(" ")[2];
+
+       return (formattedOne.equals(formattedTwo) & formattedTwo.equals(formatted3));
+    }
+
+    public Boolean checkRedColor(WebElement e){
+        String campOld = e.getCssValue("color");
+        String c = campOld.substring(campOld.indexOf("("), campOld.indexOf(")")).replaceAll(",", "");
+        String campFormattedTwo =c.split(" ")[1];
+        String campFormatted3 =c.split(" ")[2];
+        return campFormatted3.equals(campFormattedTwo);
+    }
+
+    public WebDriver getDriver (ArrayList <WebDriver> list) {
+
+        for (int l = 0; l < list.size(); l++) {
+            System.out.println("Testing in Browser: " + list.get(l));
+            WebDriver driver = list.get(l);
+
+
+        }
+        return driver;
     }
 
 
